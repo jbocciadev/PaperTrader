b20685e43bd2ce6de7fd412692709edaf171b4fa
jbocciadev <jboccia@gmail.com>
2026-06-09 15:39:34 +0100

    Fix implementation of initial API call for finnhub in market-service.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PaperTrader
 
 GitHub repository for PaperTrader, a paper trading application.
+
+Notes:
+
+- .env:
+    - Implemented .env file for dotenv to manage environment variables that will not be shared to git/github (.included in .gitignore file).
+    - NODE_TLS_REJECT_UNAUTHORIZED=0 This line was added to avoid rejcetion due to failed certificate. This is a workaround that would need to be resolved in a production envirnment with certificates.
+
+- market-service:
+    - Changed from Alpha vantage to finnhub as there was a strict limit usage for the free tier service.
+    - Implemented the "cURL" query version for the symbol quotes so as to avoid version compatibility issues with finnhub's own js package.
--- market-service/app.js
+++ market-service/app.js
@@ -1,7 +1,47 @@
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config({
     path: "../.env",
 });
 
+// Finnhub API client setup
+const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
+
 console.log("API key is: ", process.env.FINNHUB_API_KEY);
+
+const tickers = [
+    "AAPL",
+    "NVDA",
+    "TSLA",
+    "AAPL",
+    "AMZN",
+    "MSFT",
+    "META",
+    "GOOGL",
+    "AMD",
+    "PLTR",
+    "INTC",
+];
+
+const opening_values = {};
+
+async function openingPrices() {
+    try {
+        console.log("polling prices...");
+        for (var ticker of tickers) {
+            console.log(ticker);
+            // Source: https://finnhub.io/docs/api/quote
+            const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB_API_KEY}`;
+            const response = await axios.get(url);
+
+            // console.log(response.data);
+            opening_values[ticker] = response.data;
+        }
+        console.log("responses: \n\n", opening_values);
+    } catch (error) {
+        console.error("Error fetching initial ticker prices.", error.message);
+    }
+}
+
+openingPrices();
