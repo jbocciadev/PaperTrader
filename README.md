29268e6018116621921c69857c7aff0902d1724b
jbocciadev <jboccia@gmail.com>
2026-06-09 16:06:23 +0100

    Implement concurrent calls for tickers in finnhub api.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Notes:
 - .env:
     - Implemented .env file for dotenv to manage environment variables that will not be shared to git/github (.included in .gitignore file).
     - NODE_TLS_REJECT_UNAUTHORIZED=0 This line was added to avoid rejcetion due to failed certificate. This is a workaround that would need to be resolved in a production envirnment with certificates.
+    - TICKERS was added as an environment variable to add flexibility, decoupling it from the hardcoded.
 
 - market-service:
     - Changed from Alpha vantage to finnhub as there was a strict limit usage for the free tier service.
--- market-service/app.js
+++ market-service/app.js
@@ -8,37 +8,43 @@ dotenv.config({
 // Finnhub API client setup
 const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
 
-console.log("API key is: ", process.env.FINNHUB_API_KEY);
+// Declare variable with stock symbols (tickers).
+const tickers = process.env.TICKERS.split(",");
 
-const tickers = [
-    "AAPL",
-    "NVDA",
-    "TSLA",
-    "AAPL",
-    "AMZN",
-    "MSFT",
-    "META",
-    "GOOGL",
-    "AMD",
-    "PLTR",
-    "INTC",
-];
-
-const opening_values = {};
+// Declare empty object that will capture all ther eturned values
+const openingValues = {};
 
 async function openingPrices() {
     try {
         console.log("polling prices...");
-        for (var ticker of tickers) {
-            console.log(ticker);
-            // Source: https://finnhub.io/docs/api/quote
-            const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB_API_KEY}`;
-            const response = await axios.get(url);
+        // Traverse through values in tickers and call the finnhub api for market information.
 
-            // console.log(response.data);
-            opening_values[ticker] = response.data;
+        // Map tickers array into an array of unresolved Axios promises
+        const promises = tickers.map((ticker) => {
+            // Source: https://finnhub.io/docs/api/quote => see cURL path
+            const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB_API_KEY}`;
+            return axios.get(url).then((response) => ({
+                ticker,
+                data: response.data,
+            }));
+        });
+
+        // Fire all axios calls simultaneously
+        const results = await Promise.all(promises);
+
+        // Parse responses into object
+        for (let result of results) {
+            openingValues[result.ticker] = result.data;
         }
-        console.log("responses: \n\n", opening_values);
+
+        // for (let ticker of tickers) {
+        //     // Source: https://finnhub.io/docs/api/quote => see cURL path
+        //     const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${FINNHUB_API_KEY}`;
+        //     const response = await axios.get(url);
+
+        //     openingValues[ticker] = response.data;
+        // }
+        console.log("responses: \n\n", openingValues);
     } catch (error) {
         console.error("Error fetching initial ticker prices.", error.message);
     }
