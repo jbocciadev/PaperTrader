9c5e9078ddf03cae0cc8d23645655bc46f9820f7
jbocciadev <jboccia@gmail.com>
2026-06-24 15:13:34 +0100

    Implement gRPC client channel in FastAPI server.

--- web-server/app.py
+++ web-server/app.py
@@ -1,10 +1,36 @@
+import grpc
+import engine_pb2
+import engine_pb2_grpc
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
+# Placeholder for grpc client reference
+grpc_client = {}
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    """
+    Manages opening and closing of gRPC pipe on server startup and shutdown
+    """
+
+    # Create channel
+    channel = grpc.insecure_channel("localhost:50051")
+
+    grpc_client["stub"] = engine_pb2_grpc.TradingServiceStub(channel)
+    print("gRPC client channel established successfully.")
+
+    yield
+
+    # Cleanup connection on server shutdown
+    channel.close()
+    print("gRPC client channel terminated successfully.")
 
 # Initialize app from FastAPI class
 app = FastAPI(
     title="Paper trader - Web gateway",
     description="Public interface to trade actions.",
-    version="1.0.0"
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 # Define home route
@@ -16,5 +42,6 @@ def read_root():
     return {
         "status": "online",
         "service": "web-gateway",
-        "message": "Welcome to PaperTrader."
+        # "message": "Welcome to PaperTrader."
+        "grpc-bridge": "initialized" if "stub" in grpc_client else "offline"
     }
