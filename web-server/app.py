4fb4341fe2bed8a4fb37bbd269bf3fc4e8491525
jbocciadev <jboccia@gmail.com>
2026-06-24 16:10:57 +0100

    Implement FastAPI route for transaction routing to gRPC.

--- web-server/app.py
+++ web-server/app.py
@@ -1,6 +1,8 @@
 import grpc
+# gRPC stubs
 import engine_pb2
 import engine_pb2_grpc
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
@@ -9,11 +11,13 @@ grpc_client = {}
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
+    # Reference: https://medium.com/@marcnealer/fastapi-after-the-getting-started-867ecaa99de9
     """
     Manages opening and closing of gRPC pipe on server startup and shutdown
     """
 
-    # Create channel
+    # Establish gRPC channel
+    # Reference: https://realpython.com/python-microservices-grpc/
     channel = grpc.insecure_channel("localhost:50051")
 
     grpc_client["stub"] = engine_pb2_grpc.TradingServiceStub(channel)
@@ -45,3 +49,45 @@ def read_root():
         # "message": "Welcome to PaperTrader."
         "grpc-bridge": "initialized" if "stub" in grpc_client else "offline"
     }
+
+from pydantic import BaseModel, Field
+
+# Define the model against which data will be validated
+# Reference: https://fastapi.tiangolo.com/#requirements
+class TradeRequestModel(BaseModel):
+    user_id: int
+    ticker: str = Field(..., min_length=1, max_length=5) # string of up to 5 chars
+    quantity: int = Field(..., gt=0) # integer greater than 0
+    trade_type: str 
+
+@app.post("/trade")
+def handle_trade_route(payload: TradeRequestModel):
+
+    # Transform the request into a gRPC packet
+    if payload.trade_type.upper == "BUY":
+        chosen_type = engine_pb2.BUY
+    elif payload.trade_type.upper == "SELL":
+        chosen_type = engine_pb2.SELL
+    else:
+        return {"success": False, "message": "Invalid trade type (only BUY or SELL are allowed)."}
+    # Build the packet
+    grpc_request = engine_pb2.TradeRequest(
+        user_id=payload.user_id,
+        ticker=payload.ticker.upper(),
+        quantity=payload.quantity,
+        trade_type=chosen_type
+    )
+
+    try:
+        # Send the packet down the pipeline and capture the response
+        engine_response = grpc_client["stub"].ExecuteTrade(grpc_request)
+
+        # Return response to the user browser
+        return {
+            "success": engine_response.success,
+            "message": engine_response.message,
+            "transaction_id": engine_response.transaction_id,
+            "execution_rice": engine_response.execution_price
+        }
+    except Exception as error:
+        return {"success": False, "message": f"Could not connect to the engine: {str(error)}"}
