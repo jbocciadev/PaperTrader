f6c37fa0bf631813c88fb3e306b85339ef74d24c
Juan A. Boccia <jboccia@gmail.com>
2026-07-28 14:34:53 +0100

    Implement larger market watch card and flashing elements.

--- web-server/app/static/js/ticker-stream.js
+++ web-server/app/static/js/ticker-stream.js
@@ -46,12 +46,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 void priceElement.offsetWidth; // Force a synchronous layout engine DOM reflow to clear memory state
 
                 if (newPrice > oldPrice) {
-                    priceElement.classList.add("flash-up");
+                    priceElement.parentElement.parentElement.classList.add(
+                        "flash-up"
+                    );
+                    // priceElement.classList.add("flash-up");
                 } else {
-                    priceElement.classList.add("flash-down");
+                    priceElement.parentElement.parentElement.classList.add(
+                        "flash-down"
+                    );
+                    // priceElement.classList.add("flash-down");
                 }
                 setTimeout(() => {
-                    priceElement.classList.remove("flash-up", "flash-down");
+                    priceElement.parentElement.parentElement.classList.remove(
+                        "flash-up",
+                        "flash-down"
+                    );
+                    // priceElement.classList.remove("flash-up", "flash-down");
                 }, 1000);
             }
             priceCache[ticker] = newPrice;
